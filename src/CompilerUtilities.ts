abe088d36a3b7e03ae00a4f43c5dc65cffac11bf
Ryan Elian <ryan.elian.cs@gmail.com>
2018-05-16 03:24:10 +0700

    Remove code related to worker-farm.

--- src/CompilerUtilities.ts
+++ src/CompilerUtilities.ts
@@ -1,7 +1,6 @@
 import chalk from 'chalk';
 import * as upath from 'upath';
 import * as fse from 'fs-extra';
-import * as WorkerFarm from 'worker-farm';
 
 import { prettyBytes } from './PrettyUnits';
 import { Shout } from './Shout';
@@ -18,30 +17,6 @@ export function outputFileThenLog(filePath: string, content: string) {
 
     Shout.timed(chalk.blue(info.base), chalk.magenta(size), chalk.grey('in ' + info.dir + '/'));
     return fse.outputFile(filePath, bundle);
-}
-
-/**
- * Runs worker in separate process, returns Promise.
- * Automatically end the worker when it is finished.
- * @param modulePath 
- * @param params 
- */
-export async function runWorkerAsync<T>(modulePath: string, params) {
-    let worker = WorkerFarm(modulePath);
-    try {
-        let p = new Promise<T>((ok, reject) => {
-            worker(params, (error, result: T) => {
-                if (error) {
-                    reject(error)
-                } else {
-                    ok(result);
-                }
-            });
-        });
-        return await p;
-    } finally {
-        WorkerFarm.end(worker);
-    }
 }
 
 /**
